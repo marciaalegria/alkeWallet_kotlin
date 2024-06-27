c586bc645fcdca364eb551e75619e63241b42d51
Marcia Alegria <ytasegundo@gmail.com>
2024-06-27 14:26:01 -0400

    evaluacion integradora 5

--- app/src/main/java/com/example/proyectoandroidstudio/LoginSignUpFragment.kt
+++ app/src/main/java/com/example/proyectoandroidstudio/LoginSignUpFragment.kt
@@ -1,43 +1,45 @@
 package com.example.proyectoandroidstudio
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation.findNavController
+import com.example.proyectoandroidstudio.databinding.FragmentLoginSignUpPageBinding
 
 
 class LoginSignUpFragment : Fragment() {
-  //  private var email: String? = null
-  //  private var password: String? = null
-/*
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            email = it.getString(EMAIL_BUNDLE)
-            password = it.getString(PASSWORD_BUNDLE)
-        }
-    }*/
+    private lateinit var binding: FragmentLoginSignUpPageBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_sign_up_page, container, false)
-    }
-/*
-    companion object {
-         const val EMAIL_BUNDLE = "email_bundle"
-         const val PASSWORD_BUNDLE = "password_bundle"
+        binding = FragmentLoginSignUpPageBinding.inflate(inflater, container, false)
 
-        @JvmStatic
-        fun newInstance(email: String, password: String) =
-            LoginSignUpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(EMAIL_BUNDLE, email)
-                    putString(PASSWORD_BUNDLE, password)
-                }
-            }
-    }*/
+        return   binding.root  }
+
+    @SuppressLint("ResourceType")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = findNavController(view)
+
+        //navegacion
+
+        val button = binding.button
+
+        val button2 = binding.button2
+
+        button.setOnClickListener {
+            navController.navigate(R.id.action_loginSignUpFragment_to_signUpPageFragment)
+        }
+
+        button2.setOnClickListener {
+            navController.navigate(R.id.action_loginSignUpFragment_to_loginPageFragment)
+        }
+    }
+
 }
